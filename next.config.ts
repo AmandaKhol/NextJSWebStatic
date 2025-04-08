81d3c07d92581475023d44cc227e4d64d2695a2d
AmandaKhol <amanda.camsua@gmail.com>
2025-04-08 16:36:21 +0200

    refactor: next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/basicBuild", // Asegura que las rutas de los recursos se incluyan correctamente en el subdirectorio
+  images: {
+    loader: "default",
+    path: "/basicBuild/_next/image", // Prefijo para las imágenes optimizadas de Next.js
+  },
   ...(process.env.NODE_ENV === "production" && {
     output: "export",
   }),

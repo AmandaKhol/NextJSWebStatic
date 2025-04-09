593b3ccfc5f2faeca50f867df28e8a121b8e2619
AmandaKhol <amanda.camsua@gmail.com>
2025-04-09 14:37:01 +0200

    feat: routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import { NextConfig } from "next";
+import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  basePath: isProd ? "/NextJSWebStatic" : "",
+  assetPrefix: isProd ? "/NextJSWebStatic" : "",
+  images: {
+    unoptimized: true,
+  },
+  ...(process.env.NODE_ENV === "production" && {
+    output: "export",
+  }),
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
